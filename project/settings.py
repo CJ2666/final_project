aafd063a29fd783ea5288155f6a931bb86e0f897
Chengqian Jiang <cj2666@columbia.edu>
2020-10-12 21:26:25 +0000

    Further revised

--- project/settings.py
+++ project/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'map',
-    'sightings.app.SightingsConfig',
+    'sightings.apps.SightingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join( BASE_DIR / 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
     }
 }
 
