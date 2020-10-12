c2b438ce17def46815a4c8adbd0fba1dd256de58
Chengqian Jiang <cj2666@columbia.edu>
2020-10-12 21:51:53 +0000

    Further changes

--- map/views.py
+++ map/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from sightings.models import Squirrel
+
+# Create views
+
+def map(request):
+	squirrels = Squirrel.objects.all()[:100]
+	context = {'squirrels': squirrels}
+	return render(request, 'map/map.html', context)
